fbb43fba0a947f64e0f9ca202267ac85d63b3535
hardope <adeyeriopeoluwa05@gmail.com>
2023-11-14 05:59:35 +0100

    hotfix

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('', welcome),
     path('books/', BooksView.as_view()),
     path('books/<str:pk>/', OneBook.as_view()),
     path('download/', DownloadView.as_view()),
